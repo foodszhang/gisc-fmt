919904e111cfb7297ed3a5e465fcd90739956224
foods <foods@foods.localdomain>
2026-07-06 16:44:24 +0800

    Limit VSC scratch runner to seed 42 core

--- scripts/run_vsc_matched_scratch.sh
+++ scripts/run_vsc_matched_scratch.sh
@@ -7,17 +7,15 @@ declare -A ROOTS=(
   [ssq_baseline_scratch_reproduce]=outputs/vsc/baseline_scratch
   [ssq_vsc_scratch_warmup]=outputs/vsc/vsc_warmup
   [ssq_vsc_scratch_nocons]=outputs/vsc/vsc_nocons
-  [ssq_vsc_scratch_consonly]=outputs/vsc/vsc_consonly
 )
 
 EXPERIMENTS=(
   ssq_baseline_scratch_reproduce
   ssq_vsc_scratch_warmup
   ssq_vsc_scratch_nocons
-  ssq_vsc_scratch_consonly
 )
 
-SEEDS=(42 43 44)
+SEEDS=(42)
 
 for seed in "${SEEDS[@]}"; do
   for exp in "${EXPERIMENTS[@]}"; do
